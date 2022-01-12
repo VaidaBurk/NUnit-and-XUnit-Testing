efdc6d56d164623163aa3b77808d3bea38bcdfdf
VaidaBurk <v.burkauskaite@gmail.com>
2022-01-12 21:35:22 +0200

    NUnit Testing Advance Concepts
    Multiple Assert; Exceptions

--- Sparky/Customer.cs
+++ Sparky/Customer.cs
@@ -1,11 +1,21 @@
-﻿namespace Sparky
+﻿using System;
+
+namespace Sparky
 {
     public class Customer
     {
         public string GreetMessage { get; set; }
+
+        public int Discount = 15;
         public string GreetAndCombineNames(string firstName, string lastName)
         {
+            if (string.IsNullOrWhiteSpace(firstName))
+            {
+                throw new ArgumentException("Empty First Name");
+            }
+
             GreetMessage = $"Hello, {firstName} {lastName}";
+            Discount = 20;
             return GreetMessage;
         }
     }
--- SparkyNUnitTest/CustomerNUnitTests.cs
+++ SparkyNUnitTest/CustomerNUnitTests.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using System;
 
 namespace Sparky
 {
@@ -24,19 +25,22 @@ namespace Sparky
             customer.GreetAndCombineNames("Ben", "Spark");
 
             //Assert
-            Assert.AreEqual(customer.GreetMessage, "Hello, Ben Spark");
-
-            Assert.That(customer.GreetMessage, Is.EqualTo("Hello, Ben Spark"));
-            Assert.That(customer.GreetMessage, Does.Contain("Ben")); //case sensitive
-            Assert.That(customer.GreetMessage, Does.Contain("ben").IgnoreCase);
-            Assert.That(customer.GreetMessage, Does.StartWith("Hello"));
-            Assert.That(customer.GreetMessage, Does.EndWith("Spark"));
-            Assert.That(customer.GreetMessage, Does.Match("Hello, [A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+"));
-            //regular expression
-            //[A-Z]{1} --first symbol is any capital from A to Z
-            //[a-z]+ --any number of letters (+ means any number)
-            //  -- space
-            // --and second word the same
+            //If there is a block of asserts in one test in case one assert fails others are not run. To run all asserts Assert.Multiple is used
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(customer.GreetMessage, "Hello, Ben Spark");
+                Assert.That(customer.GreetMessage, Is.EqualTo("Hello, Ben Spark"));
+                Assert.That(customer.GreetMessage, Does.Contain("Ben")); //case sensitive
+                Assert.That(customer.GreetMessage, Does.Contain("ben").IgnoreCase);
+                Assert.That(customer.GreetMessage, Does.StartWith("Hello"));
+                Assert.That(customer.GreetMessage, Does.EndWith("Spark"));
+                Assert.That(customer.GreetMessage, Does.Match("Hello, [A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+"));
+                //regular expression
+                //[A-Z]{1} --first symbol is any capital from A to Z
+                //[a-z]+ --any number of letters (+ means any number)
+                //  -- space
+                // --and second word the same
+            });
         }
 
         [Test]
@@ -48,6 +52,40 @@ namespace Sparky
 
             //assert
             Assert.IsNull(customer.GreetMessage);
+        }
+
+        [Test]
+        public void DiscountCheck_DefaultCustomer_ReturnsDiscountInRange()
+        {
+            int result = customer.Discount;
+            Assert.That(result, Is.InRange(10, 25));
+        }
+
+        [Test]
+        public void GreetMessage_GreetedWithoutLastName_ReturnNotNull()
+        {
+            customer.GreetAndCombineNames("Ben", "");
+
+            Assert.IsNotNull(customer.GreetMessage);
+            Assert.IsFalse(string.IsNullOrWhiteSpace(customer.GreetMessage));
+        }
+
+        [Test]
+        public void GreetChecker_EmptyFirstName_ThrowsException()
+        {
+            //option 1
+            var exceptionDetails = Assert.Throws<ArgumentException>(() => customer.GreetAndCombineNames("", "Spark"));
+            Assert.AreEqual("Empty First Name", exceptionDetails.Message);
+
+            //option 2
+            Assert.That(() => customer.GreetAndCombineNames("", "Spark"),
+                Throws.ArgumentException.With.Message.EqualTo("Empty First Name"));
+
+            //Checking if exception was thrown
+            //option 1
+            Assert.Throws<ArgumentException>(() => customer.GreetAndCombineNames("", "Spark"));
+            //option 2
+            Assert.That(() => customer.GreetAndCombineNames("", "Spark"), Throws.ArgumentException);
         }
     }
 }
